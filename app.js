22ef7c9312b6761835b0d36b7d69f45ee538da7d
Hwa Ryeong <hwaryeong1@gmail.com>
2021-01-27 17:51:01 -0600

    HRs-first-blog-app done for now

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mysql = require('mysql');
-const app = express();
 const session = require('express-session');
-
+const bcrypt = require('bcrypt');
+const app = express();
 
 
 
@@ -44,14 +44,16 @@ app.use(
 
 app.use((req, res, next) => {
     if (req.session.userId === undefined) {
-        res.locals.username = 'Person';
+        res.locals.username = 'My Friend!';
         console.log('not logged in');
         res.locals.isLoggedIn = false;
+        
     } else {
         console.log('loggedin');
         res.locals.username = req.session.username;
         res.locals.isLoggedIn = true;
         const username = req.session.username;
+        res.locals.userId = req.session.userId;
     }
     next();
 });
@@ -62,7 +64,7 @@ app.get('/', (req, res)=> {
 
 app.get('/index', (req, res) => {
     connection.query(
-        'select * from posts',
+        'select posts.*, users.username from posts join users on users.id = author_id',
         (error, results) => {
             console.log(results);
             res.render('index.ejs', {posts: results});
@@ -71,6 +73,16 @@ app.get('/index', (req, res) => {
 });
 
 
+app.get('/myposts', (req, res) => {
+    connection.query(
+        'select * from posts where author_id =?',
+        [req.session.userId],        
+        (error, results) => {
+            console.log(results);
+            res.render('myposts.ejs', {posts: results});
+        }
+    )
+});
 
 
 app.get('/new', (req, res) => {
@@ -80,8 +92,8 @@ app.get('/new', (req, res) => {
 
 app.post('/create', (req, res) => {
     connection.query(
-        'insert into posts(title, preview, content) values(?, ?, ?)',
-        [req.body.title, req.body.preview, req.body.content],
+        'insert into posts(title, preview, content, author_id) values(?, ?, ?, ?)',
+        [req.body.title, req.body.preview, req.body.content, req.session.userId],
         (error, results) => {
             console.log(results);
             res.redirect('/index');
@@ -114,9 +126,11 @@ app.get('/edit/:id', (req, res)=> {
 
 app.post('/update/:id', (req, res)=> {
     connection.query(
-        'update posts set content=? where id=?',
-        [req.body.itemName, req.params.id],
+        'update posts set content=?, preview=?, title=? where id=?',
+        [req.body.contentName, req.body.previewName, req.body.titleName, req.params.id],
         (error, results) =>{
+            console.log(error);
+            console.log(results);
             res.redirect('/index')
         }
         
@@ -142,15 +156,33 @@ app.post('/login', (req, res)=> {
             }
             
             if (results.length > 0) {
-                if(req.body.password == results[0].password) {
-                    res.session.userId = results[0].id;
-                    req.session.username = results[0].username;
-                    console.log("login success!");
-                    res.redirect('/');
-                } else {
-                    console.log('password is incorrect');
-                    res.redirect('/login');
-                }
+
+                console.log('hash test:');
+                bcrypt.hash('sa', 10, (error, hash) => {
+                    console.log('attempt 1 hash: ', hash);
+                })
+                bcrypt.hash('sa', 10, (error, hash) => {
+                    console.log('attempt 2 hash: ', hash);
+                })
+                
+                bcrypt.compare(req.body.password, results[0].password, (error, isSame) => {
+                    console.log('password', req.body.password);
+                    // console.log('hash', hash);
+                    console.log('results[0]', results[0].password);
+                    if (isSame) {
+                        req.session.userId = results[0].id;
+                        req.session.username = results[0].username;
+                        console.log("login success!");
+                        res.redirect('/');
+
+                    } else {
+                        console.log('password is incorrect');
+                        res.redirect('/login');
+                    }
+                });
+
+               
+
             } else {
                 console.log('no users with that email')
                 res.redirect('/login');
@@ -160,14 +192,100 @@ app.post('/login', (req, res)=> {
 });
 
 app.get('/signup', (req, res) => {
-    res.render('signup.ejs')
+    res.render('signup.ejs', {errors: []});
+});
+
+app.post('/signup', (req, res, next) => {
+    console.log('check for empty value');
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const errors = [];
+
+    if(username === '') {
+        errors.push('Username is empty');
+    }
+
+    if(email === '') {
+        errors.push('Email is empty');
+    }
+
+    if (password === '') {
+        errors.push('Password is empty');
+    }
+
+    if (errors.length > 0) {
+        res.render('signup.ejs', {errors: errors});
+    } else {
+        next();
+    }
+},
+(req, res, next) => {
+    console.log('Check for muliple email');
+    const email = req.body.email;
+    const errors = [];
+
+    connection.query(
+        'select * from users where email = ?',
+        [email],
+        (error, results) => {
+            if (results.lenght > 0) {
+                errors.push('The email has been already used');
+                res.render('signup.ejs', { errors: errors });
+            } else {
+                next();
+            }
+         }
+    );
+},
+(req, res)=> {
+    console.log('helloooo');
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    bcrypt.hash(password, 10, (error, hash)=> 
+        connection.query(
+            'insert into users(username, email, password) values (?, ?, ?)',
+            [username, email, hash],
+            (error, results) => {
+                console.log('hash', hash);
+                console.log(error);
+                req.session.userId = results.insertId;
+                req.session.username = username;
+                res.redirect('/');
+            }
+        )
+    )
 });
 
 app.get('/login', (req, res) => {
-    req.session.destroy((error)=> {
+    req.session.destroy(error=> {
         res.redirect('/top');
 
     });
 });
 
+app.get('/content/:id', (req, res)=> {
+    
+    connection.query(
+        'select * from posts where id=?',
+        [req.params.id],
+        (error, results) => {
+            console.log(error);
+            console.log('result: ', results);
+            res.render('content.ejs', {posts: results[0]});
+        }
+    )
+});
+
+app.post('/logout', (req, res) => {
+    console.log('POST received at /login');
+    req.session.destroy(error => {
+        res.redirect('/');
+    });
+});
+
+
+
 app.listen(3000);
+
